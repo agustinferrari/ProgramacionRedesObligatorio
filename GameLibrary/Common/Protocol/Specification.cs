2ccdb3f86e061404b6d1385cf9206be7d2c8a7ff
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-09-29 16:23:37 -0300

    Removes unnecessary comments

--- GameLibrary/Common/Protocol/Specification.cs
+++ GameLibrary/Common/Protocol/Specification.cs
@@ -6,6 +6,6 @@ namespace Common.Protocol
     {
         public const int FixedFileNameLength = 4;
         public const int FixedFileSizeLength = 8;
-        public const int MaxPacketSize = 32768; // 32KB
+        public const int MaxPacketSize = 32768;
     }
 }
