910ddcc620b9f525ad8af82f42a86d6edb007468
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-16 10:03:43 -0300

    Comments grpc controller on serer admin

--- GameLibrary/ServerAdmin/GrpcControllers/GameGrpc.cs
+++ GameLibrary/ServerAdmin/GrpcControllers/GameGrpc.cs
@@ -12,18 +12,14 @@ namespace ServerAdmin.GrpcControllers
         //     AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",true);
         //     Console.WriteLine("Starting gRPC client example......");
         //     var channel = GrpcChannel.ForAddress("http://localhost:5001");
-        //     var client = new Greeter.GreeterClient(channel);
-        //     var response =  await client.SayHelloAsync(new HelloRequest{Name = "Hola"});
+        //     var client = new GameProto.GameProtoClient(channel);
+        //     
+        //     var response =  await client.GetGames(new GamesRequest(){Command = "GetGames", User = "Juan"});
         //     Console.WriteLine("Respuesta: " + response.Message);
         //     
-        //     Console.WriteLine("Ingrese un numero:");
-        //     var numberParam = int.Parse(Console.ReadLine());
-        //     var numberResponse = await client.GiveMeANumberAsync(new NumberRequest {NumberParameter = numberParam});
-        //     Console.WriteLine("Respuesta del number: " + numberResponse.NumberResult);
-        //
-        //     var userList = await client.GiveMeUsersAsync(new UserRequest());
+        //     var gameList = await client.GetGames(new UserRequest());
         //     
-        //     foreach (var userListUser in userList.Users)
+        //     foreach (var userListUser in gameList.Games)
         //     {
         //         Console.WriteLine("User Name: " + userListUser.UserName + ", User Age: " + userListUser.UserAge);
         //     }
