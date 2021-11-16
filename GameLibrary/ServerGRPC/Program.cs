3afa6e4980b608f14a2ad233a11a8d1fc014a739
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-16 09:21:06 -0300

    Adds serverAdmin WebAPI

--- GameLibrary/ServerGRPC/Program.cs
+++ GameLibrary/ServerGRPC/Program.cs
@@ -28,8 +28,7 @@ namespace ServerGRPC
                 serverNetworkStreamHandler.CreateClientConectionTask();
 
                 ServerMenuRenderer.LoadMainMenu();
-                //Task.run
-                ServerMenuHandler.HandleMainMenuResponse(serverNetworkStreamHandler);
+                Task.Run(() =>  ServerMenuHandler.HandleMainMenuResponse(serverNetworkStreamHandler));
             }
             else
             {
@@ -37,9 +36,7 @@ namespace ServerGRPC
             }
             CreateHostBuilder(args).Build().Run();
         }
-
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
