f2e6c5f9bbb9980128612eccbaf7e6bd620e3626
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-21 23:53:17 -0300

    Removes unnecessary send data throguh rabbit methog

--- GameLibrary/ServerLogs/Services/RabbitMQService/IBus.cs
+++ GameLibrary/ServerLogs/Services/RabbitMQService/IBus.cs
@@ -5,8 +5,6 @@ namespace ServerLogs.Services.RabbitMQService
 {
     public interface IBus
     {
-        Task SendAsync<T>(string queue, T message);
-
         Task ReceiveAsync<T>(string queue, Action<T> onMessage);
     }
 }
